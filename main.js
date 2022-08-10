318b8af1a1e27f7c0f9eecf5504eddb767bab92a
Kao Lo <kaochilanlo@gmail.com>
2022-08-09 23:14:17 -0400

    pruebas con la api Pokeapi

---
+++ main.js
@@ -0,0 +1,35 @@
+const baseUrl="https://pokeapi.co/api/v2/pokemon";
+
+const appNode= document.querySelector('#app');
+
+window
+    .fetch(`${baseUrl}/8`)
+    .then(respuesta=>respuesta.json())
+    .then((data)=>{
+        
+        const name=document.createElement('h2');
+        name.textContent=data.name;
+
+        const id= document.createElement('h3');
+        id.textContent=data.id;
+
+        const imagen=document.createElement('img');
+        imagen.src= data.sprites.front_default;
+
+        const tituloAbilities=document.createElement('h3');
+        tituloAbilities.textContent="Abilities:";
+
+        const abilities= document.createElement('ul');
+        //let abilitiesArray=[];
+        data.abilities.forEach(item=>{
+            let li=document.createElement('li');
+            //let litText=document.createTextNode(ability.name)
+            li.textContent=item.ability.name;
+
+            abilities.append(li)
+        });
+        
+        const container=document.createElement('div')
+        container.append(name,id, imagen, tituloAbilities ,abilities)
+        appNode.append(container);
+    });
